64a672799b31fe5a6a70ee468b0d4376475a6c02
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2023-10-09 21:22:43 +0700

    latihan 5 percobaan 1

---
+++ DoWhileCuti08.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DoWhileCuti08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti : ");
+        jatahCuti = sc.nextInt();
+
+        do{
+            System.out.print("Apakah Anda ingin mengambil cuti  (y/t) ?");
+            konfirmasi = sc.next();
+            
+            if(konfirmasi.equalsIgnoreCase("y")){
+                System.out.print("Jumlah Hari : ");
+                jumlahHari = sc.nextInt();
+
+                if(jumlahHari <= jatahCuti){
+                    jatahCuti -= jumlahHari;
+                    System.out.print("Sisa jatah cuti : "+jatahCuti);
+
+                }else{
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+            }
+        }while(jatahCuti > 0);
+    }
+}
